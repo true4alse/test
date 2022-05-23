b90eb62f9f1fa61f0d82027f8ef6557af9004832
shrimp h <true4alse@naver.com>
2022-05-24 02:36:03 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Reset.css';
 
 function App() {
   return (
     <div className="wrap">
-      <div className="mainVisual">
-        <video autoplay loop muted>
-          <source src="./video/main.mp4" type="video/mp4">
-        </video>
-      </div>
-
+      <header>
+        <h1>logo</h1>
+        <ul>
+          <li>main</li>
+          <li>market</li>
+          <li>history</li>
+          <li>contact</li>
+        </ul>
+        <div className="visual">
+          <video controls>
+            <source src="/video/main2.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </header>
     </div>
   );
 }
