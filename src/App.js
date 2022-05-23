d81fc5dcd82c071a538c7a7cfbbba6afe69f1d07
shrimp h <true4alse@naver.com>
2022-05-24 01:58:14 +0900

    main change

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React TEST</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="mainVisual">
+        <video autoplay loop muted>
+          <source src="./video/main.mp4" type="video/mp4">
+        </video>
+      </div>
+
     </div>
   );
 }
